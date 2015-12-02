0070109ac10e99c73d75f5d608e786db419df6d3
Alex Rocha <arocha@motorola.com>
2015-12-02 16:10:00 -0200

    More!

--- bin/MyClass.rb
+++ bin/MyClass.rb
@@ -1,6 +1,7 @@
 require "socket"
 require "ipaddr"
 require "json"
+require "byebug"
 
 MULTICAST_ADDR = "224.0.0.1"
 BIND_ADDR = "0.0.0.0"
@@ -12,8 +13,51 @@ class MyClass
 
 	def before_show()
 		@builder['window2'].show
-		
-		Thread.new do
+    @list_view = VR::ListView.new({:name => String})
+    @builder['participants_list'].add(@list_view)
+
+    t = Thread.new do
+      socket = UDPSocket.new
+		  membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
+
+		  socket.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership)
+		  socket.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
+
+		  socket.bind(BIND_ADDR, PORT)
+
+      loop do
+        sleep(0.5)
+        to_remove = nil
+        @list_view.each_row do |row|
+          begin
+            raise "Empty" unless !row.get_value(0).empty?
+            q = Question.new
+            q.name = row.get_value(0)
+            send(q.to_json)
+            puts "Waiting response"
+            message = ""
+            (Timeout.timeout(1000) {message, _ = socket.recvfrom(255)})
+            puts "Receive response"
+            message = JSON.parse(message)
+            if message['type'] == 'answer'
+              a = Answer.from_json(message)
+              puts "Response ok for " + a.name
+            end
+          rescue
+            puts("Recv timed out for participant " + row.get_value(0))
+            if !row.get_value(0).empty? and row.get_value(0) != @name
+              to_remove = row
+            end
+          end
+        end
+        if to_remove != nil
+          #@list_view.remove to_remove
+        end
+      end
+    end
+    t.abort_on_exception = true
+
+		t2 = Thread.new do
 			puts "Created new thread"
 		  socket = UDPSocket.new
 		  membership = IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(BIND_ADDR).hton
@@ -26,11 +70,50 @@ class MyClass
 		  loop do
 					puts "Waiting message"
 		      message, _ = socket.recvfrom(255)
-					m = Message.from_json(message)
-		      @builder['messages_text_view'].buffer.insert_at_cursor("\r\nFrom: " + m.name + "\r\n" + m.message)
+					message = JSON.parse(message)
+          puts message['type']
+					case message['type']
+					when 'message'
+						m = Message.from_json(message)
+		     		@builder['messages_text_view'].buffer.insert_at_cursor("\r\nFrom: " + m.name + "\r\n" + m.message)
+          when 'participant'
+            p = Participant.from_json(message)
+            register_participant(p)
+          when 'question'
+            q = Question.from_json(message)
+            puts @name
+            if !@name.nil? and !@name.empty? and q.name == @name
+              a = Answer.new
+              a.name = @name
+              send(a.to_json)
+            else
+              if !@name.nil? and !@name.empty?
+                p = Participant.new
+                p.name = @name
+                send(p.to_json)
+              end
+            end
+					else
+						puts 'Invalid type message'
+					end
 	 		end
 		end
+    t2.abort_on_exception = true
 	end
+
+  def register_participant participant
+    flag = true
+    @list_view.each_row do |row|
+      if row.get_value(0) == participant.name
+        puts "Participant already exist."
+        flag = false
+      end
+    end
+    if flag
+      puts "Should add"
+      @list_view.add_row({:name => participant.name})
+    end
+  end
 
 	def welcome_message
 		@builder['messages_text_view'].buffer.insert_at_cursor("Welcome " + @name + "!")
@@ -38,6 +121,9 @@ class MyClass
 
 	def enter_name_button__clicked(*args)
 		@name = @builder['my_name_input'].text
+    p = Participant.new
+    p.name = @name
+    send(p.to_json)
 		welcome_message
 		@builder['window2'].destroy
 	end
@@ -47,15 +133,19 @@ class MyClass
 	end
 
 	def send_button__clicked(*args)
-		socket = UDPSocket.open
-		socket.setsockopt(:IPPROTO_IP, :IP_MULTICAST_TTL, 1)
 		message = @builder["new_message_text_view"].text
 		m = Message.new
 		m.message = message
 		m.name = @name
-		socket.send(m.to_json, 0, MULTICAST_ADDR, PORT)
-		socket.close
+    send(m.to_json)
 	end
+
+  def send data
+    socket = UDPSocket.open
+		socket.setsockopt(:IPPROTO_IP, :IP_MULTICAST_TTL, 1)
+		socket.send(data, 0, MULTICAST_ADDR, PORT)
+		socket.close
+  end
 
 end
 
@@ -63,8 +153,7 @@ class Message
 
 	attr_accessor :name, :message
 
-	def self.from_json string
-		data = JSON.parse string
+	def self.from_json data
 		_self = self.new
 		_self.name = data['name']
 		_self.message = data['message']
@@ -72,6 +161,58 @@ class Message
 	end
 
 	def to_json
-		{'name' => @name, 'message' => @message}.to_json
+		{
+			'type' => 'message',
+			'name' => @name,
+			'message' => @message
+		}.to_json
 	end
+end
+
+class Participant
+
+  attr_accessor :name
+
+  def self.from_json data
+    _self = self.new
+    _self.name = data['name']
+    _self
+  end
+
+  def to_json
+    {'type' => 'participant', 'name' => self.name}.to_json
+  end
+
+end
+
+class Question
+
+  attr_accessor :name
+
+  def self.from_json data
+    _self = self.new
+    _self.name = data['name']
+    _self
+  end
+
+  def to_json
+    {'type' => 'question', 'name' => self.name}.to_json
+  end
+
+end
+
+class Answer
+
+  attr_accessor :name
+
+  def self.from_json data
+    _self = self.new
+    _self.name = data['name']
+    _self
+  end
+
+  def to_json
+    {'type' => 'answer', 'name' => self.name}.to_json
+  end
+
 end
--- main.rb
+++ main.rb
@@ -3,12 +3,14 @@
 require 'vrlib'
 
 #make program output in real time so errors visible in VR.
-STDOUT.sync = true
-STDERR.sync = true
 
 #everything in these directories will be included
 my_path = File.expand_path(File.dirname(__FILE__))
-require_all Dir.glob(my_path + "/bin/**/*.rb") 
-
-MyClass.new.show
+require_all Dir.glob(my_path + "/bin/**/*.rb")
+begin
+  MyClass.new.show
+raise StandardError => e
+  puts "Here"
+  puts e
+end
 
